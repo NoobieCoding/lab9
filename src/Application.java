ece38c024114885f38b6776690f7e761f2f7ea3d
nuttapong rojanavanich <nuttapong rojanavanich>
2016-04-21 20:23:27 +0700

    Minor spell correction

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,7 @@
 public class Application {
 	public static void main(String[] args) {
 		UnitConverter uc = new UnitConverter();
-		ConverterUI converterUserInteface = new ConverterUI(uc);
-		converterUserInteface.run();
+		ConverterUI converterUserInterface = new ConverterUI(uc);
+		converterUserInterface.run();
 	}
 }
